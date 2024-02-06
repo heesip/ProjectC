8309cb3e9660ba329851435203bd9d8267815c1b
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2024-02-06 14:22:28 +0900

    적이 플레이어를 추적하는 코드 추가

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class Enemy : RecycleObject
 {
+    Vector2 _direction;
+    Vector2 _nextVec;
+    [SerializeField] bool isDead;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] Rigidbody2D target;
+    [SerializeField] float _speed;
+
+    private void OnEnable()
+    {
+        isDead = false;
+        rigid = GetComponent<Rigidbody2D>();
+        target = Player.Instance.GetComponent<Rigidbody2D>();
+    }
 
 
+    void Run()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        _direction = target.position - rigid.position;
+        _nextVec = _direction.normalized * _speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + _nextVec);
+        rigid.velocity = Vector2.zero;
 
+    }
 
-
-
-
-
-
-
+    private void FixedUpdate()
+    {
+        Run();
+    }
 
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -25,5 +47,5 @@ public class Enemy : RecycleObject
         Restore();
     }
 
-    
+
 }
