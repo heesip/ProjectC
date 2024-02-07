a068a39a1a19825b3e62d730bc8f8309f2b35cb1
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2024-02-07 22:44:27 +0900

    적 스크립트 추가(오브젝트풀링과쓰기 적절하게)

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -4,37 +4,43 @@ using UnityEngine;
 
 public class Enemy : RecycleObject
 {
+    #region EnemyComponent
+    Rigidbody2D _rigid;
+    SpriteRenderer _sprite;
+    Animator _anim;
+    [SerializeField] RuntimeAnimatorController[] _animCon;
+    Collider2D _coll;
+    #endregion
+
+    bool _isDead;
+    Rigidbody2D _target;
+    [SerializeField] float _speed;
+    [SerializeField] float _health;
+    [SerializeField] float _maxHealth;
     Vector2 _direction;
     Vector2 _nextVec;
-    [SerializeField] bool isDead;
-    [SerializeField] Rigidbody2D rigid;
-    [SerializeField] Rigidbody2D target;
-    [SerializeField] float _speed;
 
-    private void OnEnable()
+    void Awake()
     {
-        isDead = false;
-        rigid = GetComponent<Rigidbody2D>();
-        target = Player.Instance.GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _rigid = GetComponent<Rigidbody2D>();
+        _coll = GetComponent<Collider2D>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
-
-    void Run()
+    void OnEnable()
     {
-        if (isDead)
-        {
-            return;
-        }
-        _direction = target.position - rigid.position;
-        _nextVec = _direction.normalized * _speed * Time.deltaTime;
-        rigid.MovePosition(rigid.position + _nextVec);
-        rigid.velocity = Vector2.zero;
-
+        LiveSet();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Run();
+    }
+
+    void LateUpdate()
+    {
+        Turn();
     }
 
 
@@ -44,8 +50,51 @@ public class Enemy : RecycleObject
         {
             return;
         }
-        Restore();
+        DeadSet();
+
     }
 
+    void LiveSet()
+    {
+        _health = _maxHealth;
+        _isDead = false;
+        _coll.enabled = true;
+        _rigid.simulated = true;
+        _anim.SetBool("Dead", false);
+        _target = Player.Instance.GetComponent<Rigidbody2D>();
+    }
+
+    void DeadSet()
+    {
+        _isDead = true;
+        _coll.enabled = false;
+        _rigid.simulated = false;
+        _anim.SetBool("Dead", true);
+
+    }
+
+
+    void Run()
+    {
+        if (_isDead)
+        {
+            return;
+        }
+        _direction = _target.position - _rigid.position;
+        _nextVec = _direction.normalized * _speed * Time.deltaTime;
+        _rigid.MovePosition(_rigid.position + _nextVec);
+        _rigid.velocity = Vector2.zero;
+
+    }
+
+    void Turn()
+    {
+        if (_isDead)
+        {
+            return;
+        }
+        _sprite.flipX = _target.position.x < _rigid.position.x;
+
+    }
 
 }
