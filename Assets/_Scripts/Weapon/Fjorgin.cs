ff91f57a632c0449e54db064b88ee9af01039b77
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2024-04-16 23:49:55 +0900

    피요르긴 공격 애니메이션 추가 + 피요르긴 스크립트에 공격 예시 추가

--- Assets/_Scripts/Weapon/Fjorgin.cs
+++ Assets/_Scripts/Weapon/Fjorgin.cs
@@ -6,26 +6,82 @@ public class Fjorgin : Weapon
 {
     [SerializeField] GameObject _fjorgin;
     SpriteRenderer _fjorginSprite;
-
+    Animator _anim;
+    [SerializeField] bool _isReverse;
     //Temp Code
     [SerializeField] Vector3 _fjorginRightPos = new Vector3(0, 2.5f, 0);
     [SerializeField] Vector3 _fjorginLeftPos = new Vector3(0, 2.5f, 0);
 
-    private void Awake()
-    {
-        Initialize();
-    }
+    WaitForSeconds _coolTime = new WaitForSeconds(3);
+
+
+
     protected override void Initialize()
     {
+        _anim = GetComponent<Animator>();
+
         _fjorginSprite = _fjorgin.GetComponent<SpriteRenderer>();
         _fjorgin.transform.Translate(transform.up/*, Space.World*/);
         _rightPos = _fjorginRightPos;
         _leftPos = _fjorginLeftPos;
     }
-    private void LateUpdate()
+
+    private void Awake()
     {
-        bool isReverse = Player.Instance.IsLeft;
-        _fjorginSprite.flipX = !isReverse;
-        transform.localPosition = isReverse ? _leftPos : _rightPos;
+        Initialize();
+    }
+
+    private void OnEnable()
+    {
+        _attackCoHandle = StartCoroutine(AttackCo());
+    }
+
+    private void OnDisable()
+    {
+        StopAttackCo();
+    }
+
+
+
+    Coroutine _attackCoHandle;
+
+    IEnumerator AttackCo()
+    {
+
+        while (true)
+        {
+            yield return null;
+            AttackPosition();
+            switch (_isReverse)
+            {
+                case false:
+                    _anim.SetTrigger("isRight");
+                    break;
+                case true:
+                    _anim.SetTrigger("isLeft");
+                    break;
+            }
+            yield return _coolTime;
+
+        }
+
+
+    }
+
+    void StopAttackCo()
+    {
+        if (_attackCoHandle != null)
+        {
+            StopCoroutine(_attackCoHandle);
+        }
+    }
+
+
+
+    void AttackPosition()
+    {
+        _isReverse = Player.Instance.IsLeft;
+        _fjorginSprite.flipX = !_isReverse;
+        transform.localPosition = _isReverse ? _leftPos : _rightPos;
     }
 }
