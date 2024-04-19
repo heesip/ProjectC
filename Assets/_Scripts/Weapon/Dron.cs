3fac6d8f071bffb2f688892d30cbecfbcaf71bdc
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2024-04-19 23:00:21 +0900

    드론 공격 포인트 추가

--- Assets/_Scripts/Weapon/Dron.cs
+++ Assets/_Scripts/Weapon/Dron.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Dron : Weapon
 {
+    [SerializeField] Transform _dronAttackPoint1;
+    [SerializeField] Transform _dronAttackPoint2;
     //Temp Code
-    [SerializeField] Vector3 _dronRightPos = new Vector3(-1, 1, 0);
-    [SerializeField] Vector3 _dronLeftPos = new Vector3(1, 1, 0);
+    [SerializeField] Vector3 _dronRightPos = new Vector3(-1, 1.5f, 0);
+    [SerializeField] Vector3 _dronLeftPos = new Vector3(1, 1.5f, 0);
     [SerializeField] Quaternion _dronRightRot = Quaternion.Euler(0, 0, 75);
     [SerializeField] Quaternion _dronLeftRot = Quaternion.Euler(0, 0, -75);
 
@@ -20,6 +22,7 @@ public class Dron : Weapon
         _leftPos = _dronLeftPos;
         _rightRot = _dronRightRot;
         _leftRot = _dronLeftRot;
+
     }
 
     private void LateUpdate()
