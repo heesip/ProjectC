f107094151056d425f59a348e7036fae20cb6363
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2024-06-20 14:37:49 +0900

    아이템 박스에서 랜덤으로 포션 혹은 자석아이템이 나타나게 수정

--- Assets/_Scripts/Prefab_ObjectPool/ItemBox.cs
+++ Assets/_Scripts/Prefab_ObjectPool/ItemBox.cs
@@ -7,6 +7,42 @@ using DG.Tweening;
 public class ItemBox : RecycleObject
 {
     float _duration = 0.2f;
+    int _minNumber = 0;
+    int _maxNumber = 10;
+
+    void RandomItem()
+    {
+        int randomNumber = Random.Range(_minNumber, _maxNumber);
+        switch (randomNumber)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                Potion potion = FactoryManager.Instance.GetPotion();
+                BoxOpen(potion);
+                break;
+            case 7:
+            case 8:
+            case 9:
+                Magnet magnet = FactoryManager.Instance.GetMagnet();
+                BoxOpen(magnet);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    void BoxOpen(Item item)
+    {
+        item.transform.position = transform.position;
+        Vector3 upVector = transform.position + Vector3.up;
+        item.transform.DOMove(upVector, _duration);
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,11 +50,7 @@ public class ItemBox : RecycleObject
         {
             return;
         }
-
-        Potion potion = FactoryManager.Instance.GetPotion();
-        potion.transform.position = transform.position;
-        Vector3 upVector = transform.position + Vector3.up;
-        potion.transform.DOMove(upVector, _duration);
+        RandomItem();
         Restore();
     }
 }
